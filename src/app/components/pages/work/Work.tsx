c96f2c8c588d1e69621a224173668f3db010f402
Cristian Camilo Lopez <clopez@corenai.com>
2023-11-24 09:40:49 -0500

    Cards were added to the work section.

--- src/app/components/pages/work/Work.tsx
+++ src/app/components/pages/work/Work.tsx
@@ -1,5 +1,6 @@
 import { routes } from "../../../constants";
 import workStyle from "./Work.module.css";
+import ExpandableCard from "./card/ExpandableCard.tsx";
 
 
 export default function Work() {
@@ -8,11 +9,11 @@ export default function Work() {
   return (
     <section id={routes.work.pathRoute} className={workStyle.sectionContainer} style={{ height: "20%" }}>
       <div className={workStyle.container}>
-        <div className={workStyle.one}>One</div>
-        <div className={workStyle.one}>Two</div>
-        <div className={workStyle.one}>Three</div>
-        <div className={workStyle.one}>Four</div>
-        <div className={workStyle.one}>Five</div>
+        <ExpandableCard />
+        <ExpandableCard />
+        <ExpandableCard />
+        <ExpandableCard />
+
       </div>
     </section>
   );
