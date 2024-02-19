98dc071ef9888bbe4496716fa803cdf26d343323
CamiloLopezN <clopez@corenai.com>
2024-02-19 11:59:10 -0500

    About me implemented

--- src/app/components/pages/work/expandableCard/ExpandableCard.tsx
+++ src/app/components/pages/work/expandableCard/ExpandableCard.tsx
@@ -19,7 +19,7 @@ export default function ExpandableCard({ meImage, description }: ExpandableCardP
       onBackgroundFadeClick={() => setIsCardExpanded(false)}
       transition={{ type: "spring", stiffness: 200, damping: 20 }}
     >
-      <div style={{ backgroundColor: isCardExpanded ? "black" : "transparent" }}
+      <div style={{ backgroundColor: isCardExpanded ? "#100b31" : "transparent", boxShadow: "0 8px 28px -9px rgba(0, 0, 0, 0.75)" }}
            className={`${expandableCardStyle.card}`} onClick={() => setIsCardExpanded(!isCardExpanded)}>
         <img
           src={meImage}
--- src/app/components/pages/work/expandableCard/workCard/WorkCard.tsx
+++ src/app/components/pages/work/expandableCard/workCard/WorkCard.tsx
@@ -2,7 +2,7 @@ import { WorkCardType } from "./types/WorkCardTypes.ts";
 
 function WorkCard({ title, description }: WorkCardType) {
   return (
-    <div style={{overflow: "hidden"}}>
+    <div style={{overflow: "hidden", color: "#fff", fontSize: "1.5rem", fontWeight: "200"}}>
       <h4>{title}</h4>
       <p>{description}</p>
     </div>
